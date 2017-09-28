3b76a4a79939c6349db30d3a9021858eba0e6de0
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-09-27 23:22:16 -0300

    Node.js: Inovando com Javascript no backend - 02 Coesão e Acoplamento, organizando o código  - 07 Consultando o banco de dadoss

--- Inovando com Javascript no backend/app/routes/produtos.js
+++ Inovando com Javascript no backend/app/routes/produtos.js
@@ -1,5 +1,25 @@
 module.exports = function(app) {
     app.get('/produtos', (req, res) => {
-        res.render('produtos/lista');
+        const mysql = require('mysql');
+        const cnn = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'sasa',
+            database: 'casadocodigo_nodejs'
+        });
+
+        cnn.query('select * from livros', (err, result) => {
+            if (err) {
+                console.log(err);
+                res.json(err);
+            } else {
+                console.log(result);
+                res.json(result);
+            }
+        });
+
+        cnn.end();
+
+        //res.render('produtos/lista');
     });
 }
