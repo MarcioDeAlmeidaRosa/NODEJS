a99cf75a8beacad99cdfc498cdd2251c24fc661c
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-09-28 00:27:11 -0300

    Documentar motivo da retirada

--- Inovando com Javascript no backend/app.js
+++ Inovando com Javascript no backend/app.js
@@ -1,4 +1,6 @@
 const app = require('./config/express')();
+// removida a linha que servia somente para carregar o arquivo de rotas. 
+// var rotasProdutos = require('./app/routes/produtos')(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
