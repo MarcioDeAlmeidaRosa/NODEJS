8becb6a6922d53173c3f26ec8ea8a3e3cd70689d
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-10-04 20:55:31 -0300

    Node.js: Inovando com Javascript no backend - 04 - Content negotiation - 04 Implementando o Content Negotiation

--- Inovando com Javascript no backend/test_consumo/client_android.js
+++ Inovando com Javascript no backend/test_consumo/client_android.js
@@ -1,14 +1,32 @@
 const http = require('http');
-const conf = {
+let conf = {
     hostname: 'localhost',
     port: 3000,
     path: '/produtos',
     headers: {
-        // 'Accept': 'text/html'
-        // 'Accept': 'application/json'
-        'Accept': 'application/xml'
+        'Accept': 'text/html'
     }
 };
+http.get(conf, (res)=>{
+    res.on('data', (body)=>{
+        console.log(body+'');
+    });
+});
+
+conf.headers = {
+    'Accept': 'application/json'
+}
+
+http.get(conf, (res)=>{
+    res.on('data', (body)=>{
+        console.log(body+'');
+    });
+});
+
+conf.headers = {
+    'Accept': 'application/xml'
+}
+
 http.get(conf, (res)=>{
     res.on('data', (body)=>{
         console.log(body+'');
