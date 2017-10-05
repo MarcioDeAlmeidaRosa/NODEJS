91a8fea4dbbc9171f708d9a38503e3d3f123bd1c
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-10-04 22:57:48 -0300

    Node.js: Inovando com Javascript no backend - 05 Respeitando o accept - 05 Mantendo os dados no formulário após os erros

--- Inovando com Javascript no backend/app/routes/produtos.js
+++ Inovando com Javascript no backend/app/routes/produtos.js
@@ -30,30 +30,27 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', (req, res) => {
-        res.render('produtos/form',{livro: {}, erros: null});
+        render(res, {}, {});
     });
 
     app.get('/produtos/novo', (req, res) => {
         res.redirect('/produtos/form');
     });
 
+    function render(res, livro, erros){
+        return res.render('produtos/form', {livro, erros});
+    }
+
     app.post('/produtos', (req, res) => {
         const cnn = app.infra.connectionFactory();
         const produtoBanco = new app.infra.ProdutosBancoDAO(cnn);
         const livro = req.body;
-        console.log('livro ->', livro);
-
         req.assert('titulo', 'Título é obrigatório').notEmpty();
         req.assert('preco', 'Formato do preço é inválido').isFloat();
-        //
         let erros = req.validationErrors();
-
         if (erros){
-            console.log(erros);
-            return res.render('produtos/form', {livro, erros});
+            return render(res, livro, erros);
         }
-
-
         produtoBanco.salvar(livro, (err, result) => {
             console.log('err --> ', err);
             res.redirect('/produtos');
