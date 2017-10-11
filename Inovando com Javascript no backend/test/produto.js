9ce0ed7390c93a33d422dbeab9d20704daab6b6a
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-10-11 00:05:32 -0300

    Node.js: Inovando com Javascript no backend - 07 Testes de integração - 03 Testando a listagem de produtos

--- Inovando com Javascript no backend/test/produto.js
+++ Inovando com Javascript no backend/test/produto.js
@@ -1,6 +1,31 @@
+const http = require('http');
+
 describe('#ProdutosController', function() {
-    it('Listagem json', function() {
+    it('Listagem json', function(done) {
         console.log('Teste de verificação de listagem de json');
+        let conf = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        http.get(conf, (res) => {
+            // res.on('data', (body) => {
+            //     console.log(body + '');
+            // });
+            if (res.statusCode == 200)
+                console.log('Status OK');
+
+            if (res.headers['content-type'] == 'application/json; charset=utf-8')
+                console.log('content-type OK');
+
+            done();
+        });
+
+
     });
 
 });
