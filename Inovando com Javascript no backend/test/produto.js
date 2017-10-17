585e792df90220b6086ba0bec7d4a6614d5fbd96
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-10-17 20:56:04 -0200

    Node.js: Inovando com Javascript no backend - 07 Testes de integração - 06 Lidando com o servidor nos testes

--- Inovando com Javascript no backend/test/produto.js
+++ Inovando com Javascript no backend/test/produto.js
@@ -1,31 +1,37 @@
-const http = require('http');
-const assert = require('assert');
+// const http = require('http');
+// const assert = require('assert');
+
+const app = require('../config/express')();
+const request = require('supertest')(app);
 
 describe('#ProdutosController', function() {
     it('Listagem json', function(done) {
         console.log('Teste de verificação de listagem de json');
-        let conf = {
-            hostname: 'localhost',
-            port: 3000,
-            path: '/produtos',
-            headers: {
-                'Accept': 'application/json'
-            }
-        };
+        // let conf = {
+        //     hostname: 'localhost',
+        //     port: 3000,
+        //     path: '/produtos',
+        //     headers: {
+        //         'Accept': 'application/json'
+        //     }
+        // };
 
-        http.get(conf, (res) => {
-            assert.equal(res.statusCode, 200);
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-            // if (res.statusCode == 200)
-            //     console.log('Status OK');
+        // http.get(conf, (res) => {
+        //     assert.equal(res.statusCode, 200);
+        //     assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+        //     // if (res.statusCode == 200)
+        //     //     console.log('Status OK');
 
-            // if (res.headers['content-type'] == 'application/json; charset=utf-8')
-            //     console.log('content-type OK');
+        //     // if (res.headers['content-type'] == 'application/json; charset=utf-8')
+        //     //     console.log('content-type OK');
 
-            done();
-        });
+        //     done();
+        // });
 
-
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('content-type', /json/)
+            .expect(200, done);
     });
 
 });
