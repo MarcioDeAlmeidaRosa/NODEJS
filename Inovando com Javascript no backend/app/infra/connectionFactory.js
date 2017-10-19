8343e441c32a7d83094efb8d0a8c785d39e632e9
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-10-18 23:41:07 -0200

    Node.js: Inovando com Javascript no backend - 10 Deploy - 03 Configurando o banco de dados remoto

--- Inovando com Javascript no backend/app/infra/connectionFactory.js
+++ Inovando com Javascript no backend/app/infra/connectionFactory.js
@@ -7,7 +7,7 @@ function createDBConnection() {
             host: 'localhost',
             user: 'root',
             password: 'sasa',
-            database: 'casadocodigo_nodejs2'
+            database: 'casadocodigo_nodejs'
         });
     }
 
@@ -19,6 +19,23 @@ function createDBConnection() {
             database: 'casadocodigo_nodejs_test'
         });
     }
+
+    if (process.env.NODE_ENV == 'production') {
+        let urlCnn = process.env.CLEARDB_DATABASE_URL;
+        let grupos = urlCnn.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+        console.log(urlCnn);
+        console.log('grupos[1] --> ', grupos[1]);
+        console.log('grupos[2] --> ', grupos[2]);
+        console.log('grupos[3] --> ', grupos[3]);
+        console.log('grupos[4] --> ', grupos[4]);
+        return mysql.createConnection({
+            host: grupos[3],
+            user: grupos[1],
+            password: grupos[2],
+            database: grupos[4]
+        });
+    }
+
 }
 //wrapper, pois o express-load já executava a construção da função, 
 //agora ao excecutar a construção, será devolvido outra função
